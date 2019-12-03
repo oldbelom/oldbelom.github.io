bdb4e6336f853363b013d0ce5adbe8706a8d3a5c
oldbelom <oldbelom@gmail.com>
2019-12-04 00:10:13 +0300

    add particles bg

--- gulpfile.js
+++ gulpfile.js
@@ -23,12 +23,8 @@ gulp.task('script', function(){
   return gulp.src([
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-    'node_modules/rateyo/src/jquery.rateyo.js',
-    'node_modules/mixitup/dist/mixitup.js',
-    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
-    'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
-    'node_modules/wow.js/dist/wow.js',
+    'node_modules/particles.js/particles.js'
   ])
   .pipe(concat('libs.min.js'))
   .pipe(uglify())
@@ -39,13 +35,8 @@ gulp.task('style', function(){
   return gulp.src([
     'node_modules/slick-carousel/slick/slick.css',
     'node_modules/magnific-popup/dist/magnific-popup.css',
-    'node_modules/rateyo/src/jquery.rateyo.css',
     'node_modules/normalize.css/normalize.css',
-    'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
-    'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
-    'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css',
-    'node_modules/animate.css/animate.css',
   ])
   .pipe(concat('libs.min.css'))
   .pipe(cssmin())
